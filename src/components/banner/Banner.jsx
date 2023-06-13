09c64ba65849d2b7a2d7bce7ce3f534508059e49
parthcs-secondary <parthcs626@gmail.com>
2023-06-14 01:39:54 +0530

    Added:     - navbar for mobiles     - changed banner image and styling completely

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import './Banner.css'
+import React from "react";
+import "./Banner.css";
 
 function Banner() {
   return (
     <div className="banner_image_container">
-    <div className='banner__container'>
-    {/* <div className='banner_company__name'>Routes De India</div> */}
-      <div className='tag__line'>Explore. <b id='banner__word__dream'>Dream</b>. Discover.</div>
-      <p className='banner__quote'>Life is too short for boring routines. Let's explore, laugh, and create unforgettable stories</p>
-      <button className="find__your__trip__button">
-        Find Your trip
-      </button>
+
+
+      <div className="banner__container">
+
+      
+        {/* <div className='banner_company__name'>Routes De India</div> */}
+        <div className="tag__line">
+          Explore. <b id="banner__word__dream">Dream</b>. Discover.
+        </div>
+        <p className="banner__quote">
+          Life is too short for boring routines. Let's explore, laugh, and
+          create unforgettable stories
+        </p>
+        <button className="find__your__trip__button">Find Your trip</button>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,61 @@
-import React from "react";
-import "./Navbar.css"
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+import {HiMenuAlt3} from 'react-icons/hi'
 function Navbar() {
+  const [hamClicked, setHamClicked] = useState(true);
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
+
+  useEffect(() => {
+    // Function to handle window resize
+    // if window is <= 900 which is mobile view ,
+    // flag will be false - list will be not shown
+    if (windowSize <= 900) {
+      setHamClicked(false);
+    } else {
+      setHamClicked(true);
+    }
+
+    const handleResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [windowSize]);
   return (
     <nav className="navbar">
       <div className="navbar__container">
-        <h1 className="CompanyName">RoutesDe'<b id="navbar__india">India</b></h1>
+        <h1 className="CompanyName">
+          RoutesDe'<b id="navbar__india">India</b>
+        </h1>
         <div className="menu__list__container">
-          <ul className="menu__list">
-            <li>
-              <a href="">SPITI</a>
-            </li>
-            <li>
-              <a href="">BACKPACKING TRIPS</a>
-            </li>
-            <li>
-              <a href="">WEEKEND GETAWAYS</a>
-            </li>
-            <li>
-              <a href="">TREKS</a>
-            </li>
-          </ul>
+          {hamClicked && (
+            <ul className="menu__list">
+              <li>
+                <a href="">Spiti</a>
+              </li>
+              <li>
+                <a href="">Backpacking Trips</a>
+              </li>
+              <li>
+                <a href="">Weekend Getaways</a>
+              </li>
+              <li>
+                <a href="">Treks</a>
+              </li>
+            </ul>
+          )}
+          <div
+            className="hamburger toggle__nav"
+            onClick={() => setHamClicked(!hamClicked)}
+          >
+            <HiMenuAlt3 style={{fontSize: '1.1em'}}/>
+          </div>
         </div>
       </div>
     </nav>
