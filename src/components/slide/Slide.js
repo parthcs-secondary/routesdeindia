dbd98e0c69d978db8660544172e6f72f9b088caf
AmitGaikar00 <amitgaikar6025@gmail.com>
2023-06-15 20:28:10 +0530

    made changes in Banner01 zoom issue. Trip Carousel in progress

--- src/components/slide/Slide.js
+++ src/components/slide/Slide.js
@@ -1,95 +1,18 @@
 import React from "react";
-import Slider from "react-slick";
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
-import { GrPrevious, GrNext } from "react-icons/gr";
+// import { GrPrevious, GrNext } from "react-icons/gr";
 import "./Slide.css";
 
-const CustomPrevArrow = ({ onClick }) => (
-  <div className="prev-arrow" onClick={onClick}>
-    <GrPrevious />
-  </div>
-);
-
-const CustomNextArrow = ({ onClick }) => (
-  <div className="next-arrow" onClick={onClick}>
-    <GrNext />
-  </div>
-);
-
-const Slide = ({ heading, children ,flag}) => {
-
-  const reviewCardSeettings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: false,
-    autoplay: false,
-    prevArrow: <CustomPrevArrow />, // Custom previous arrow component
-    nextArrow: <CustomNextArrow />, // Custom next arrow component
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
-
-  const tripCardSeettings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 3,
-    centerMode: false,
-    autoplay: false,
-    prevArrow: <CustomPrevArrow />, // Custom previous arrow component
-    nextArrow: <CustomNextArrow />, // Custom next arrow component
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
-
-  let settings;
-  const background ={
-    backgroundImage: "linear-gradient(white , rgb(5,187,32))"
-  } 
-  if(flag ==='card'){
-    settings = {...reviewCardSeettings} 
-  }else{
-    settings = {...tripCardSeettings}
-    background.backgroundImage = "white";
-  }
-   
-
+const Slide = ({ heading, children }) => {
   return (
-    <div className="slide" style={background}>
-      <h1>{heading}</h1>
-      <div className="slide__container">
-        <Slider {...settings}>{children}</Slider>
+      <div className="slide">
+        <div className="slide__container">
+          <div className="slide__title">
+            <div className="heading">{heading}</div>
+          </div>
+          <div className="slide__cards">{children}</div>
+        </div>
       </div>
-    </div>
+
   );
 };
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
+import './Home.css'
 import Banner from "../../components/banner/Banner";
 import Slide from "../../components/slide/Slide";
-import ReviewCard from "../../components/reviewCard/ReviewCard";
+// import ReviewCard from "../../components/reviewCard/ReviewCard";
 import TripCard from "../../components/tripCard/TripCard";
 import { cards, trips } from "../../assets/data/data";
 import Banner01 from "../../components/banner01/Banner01";
@@ -9,24 +10,28 @@ function Home() {
   return (
     <div className="Home">
       <Banner />
-      <Banner01/>
+      <Banner01 />
+      
       {/* upcoming trips  */}
+
       <div className="tours">
-        <Slide heading={"Upcoming Trips"} flag={"trip"}>
+        <Slide heading={"Upcoming Trips"}>
           {trips.map((card) => (
-            <TripCard key={card.id} card={card} />
+            <div className="trip__card">
+              <TripCard key={card.id} card={card} />
+            </div>
           ))}
         </Slide>
       </div>
 
       {/* Review slider  */}
-      <div className="Reviews">
+      {/* <div className="Reviews">
         <Slide heading={"See what others say about us."} flag={"card"}>
           {cards.map((card) => (
             <ReviewCard key={card.id} card={card} />
           ))}
         </Slide>
-      </div>
+      </div> */}
     </div>
   );
 }
