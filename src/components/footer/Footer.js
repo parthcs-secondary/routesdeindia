bb4db47090eefa36458ea67c39dec97103dd7382
AmitGaikar00 <amitgaikar6025@gmail.com>
2023-06-16 03:20:23 +0530

    modified:     -modified slide component and its styling     -modified tripCard and reviewCard     -modified footer and footeritems

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,32 +11,41 @@ import { footerData } from "../../assets/data/data";
 import FooterItem from "../footerItem/FooterItem";
 
 function Footer() {
-  
   return (
     <footer className="footer">
       <div className="footer__container">
-      <hr />
+        
+        {/* line break */}
+        <hr className="footer__linebreak"/>
+
+        
         <div className="footer__top">
           {/* // footer items starts here */}
 
           {footerData.map((card) => (
-            <FooterItem key={card.id} heading={card.heading} list={card.items}/>
+            <FooterItem
+              key={card.id}
+              heading={card.heading}
+              list={card.items}
+            />
           ))}
 
           {/* footer items ends here */}
         </div>
-        <hr />
+
+        <hr className="footer__linebreak"/>
+
         <div className="footer__bottom">
           <div className="footer__left">
-            <h2>© 2023 RoutesDeIndia. All rights reserved.</h2>
+            <h2 className="footer__left__text">© 2023 RoutesDeIndia. All rights reserved.</h2>
           </div>
           <div className="footer__right">
             <div className="social">
-              <BsFacebook />
-              <BsInstagram />
-              <BsPinterest />
-              <BsLinkedin />
-              <BsYoutube />
+              <BsFacebook  className="social__icons"/>
+              <BsInstagram className="social__icons"/>
+              <BsPinterest className="social__icons"/>
+              <BsLinkedin className="social__icons"/>
+              <BsYoutube className="social__icons"/>
             </div>
           </div>
         </div>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,20 +2,31 @@ import React from "react";
 import Banner from "../../components/banner/Banner";
 import Slide from "../../components/slide/Slide";
 import ReviewCard from "../../components/reviewCard/ReviewCard";
-import './Home.css'
+import "./Home.css";
 import TripCard from "../../components/tripCard/TripCard";
 import { cards, trips } from "../../assets/data/data";
 import Banner01 from "../../components/banner01/Banner01";
+
 function Home() {
   return (
     <div className="Home">
       <Banner />
       <Banner01 />
-      
-      {/* upcoming trips  */}
 
+      {/* upcoming trips  */}
       <div className="tours">
         <Slide heading={"Upcoming Trips"}>
+          {trips.map((card) => (
+            <div className="trip__card">
+              <TripCard key={card.id} card={card} />
+            </div>
+          ))}
+        </Slide>
+      </div>
+
+      {/* others  */}
+      <div className="tours">
+        <Slide heading={"Leh Ladakh"}>
           {trips.map((card) => (
             <div className="trip__card">
               <TripCard key={card.id} card={card} />
@@ -28,7 +39,9 @@ function Home() {
       <div className="Reviews">
         <Slide heading={"See what others say about us."} flag={"card"}>
           {cards.map((card) => (
-            <ReviewCard key={card.id} card={card} />
+            <div className="review__card">
+              <ReviewCard key={card.id} card={card} />
+            </div>
           ))}
         </Slide>
       </div>
